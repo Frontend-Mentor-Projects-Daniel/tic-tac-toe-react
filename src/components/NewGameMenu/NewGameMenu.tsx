da539bfe667898d0417b2c3faa713f8b097cb0ca
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2023-03-08 16:38:23 -0500

    set up base design system

--- src/components/NewGameMenu/NewGameMenu.tsx
+++ src/components/NewGameMenu/NewGameMenu.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
 
+import xImageOutline from '../../assets/icon-x-outline.svg';
+import oImageOutline from '../../assets/icon-o-outline.svg';
+
 /**
  * Renders the initial game board
  */
@@ -10,6 +13,24 @@ function NewGameMenu() {
       {/* Logo */}
       <div>
         <Logo width={72} height={32} />
+      </div>
+
+      {/* Mark */}
+      <div className='bg-mark-bg'>
+        <h2>Pick Player 1's Mark</h2>
+
+        <div>
+          <img src={xImageOutline} alt='Press this to choose X' />
+          <img src={oImageOutline} alt='Press this to choose O' />
+        </div>
+
+        <p>Remember: X goes first</p>
+      </div>
+
+      {/* Play Buttons */}
+      <div>
+        <button>New Game (vs cpu)</button>
+        <button className='bg-player-bg'>New Game (vs player)</button>
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,17 @@ module.exports = {
     preflight: false,
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+      colors: {
+        'body-bg': 'hsl(var(--clr-body-bg) / <alpha-value>)',
+        'mark-bg': 'hsl(var(--clr-mark-bg) / <alpha-value>)',
+        'player-bg': 'hsl(var(--clr-player-bg) / <alpha-value>)',
+        'cpu-or-p2-bg': 'hsl(var(--clr-cpu-or-p2-bg) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 };
